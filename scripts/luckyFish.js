54165b09e92a3aae670e547313678b8f68178f21
党弘扬 <dhy.since2009@gmail.com>
2012-06-28 22:25:48 +0800

    主框架

---
+++ scripts/luckyFish.js
@@ -0,0 +1,13 @@
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Administrator
+ * Date: 12-6-28
+ * Time: 下午10:18
+ * find out interest
+ */
+
+var luckyFish = {
+    init: function(users){
+        this._users = users;
+    }
+};
---
+++ scripts/users.js
@@ -0,0 +1,11 @@
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Administrator
+ * Date: 12-6-28
+ * Time: 下午10:21
+ * find out interest
+ */
+
+var users = [
+
+];
